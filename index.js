01925d181b804f521c54663f11dfaf915891f933
ebrahemmsd <ebrahemmosaad87@gmail.com>
2023-08-08 17:29:34 +0300

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+var test = 40
+console.log('test')
+
+// var age = 38
+//  if(age == 40){
+//     console.log("you are a Man!!!!!")
+// }else if(age > 40){
+//     console.log("you are too old")
+//  }
+//  else{
+//     console.log("you still young, best wishes")
+//  }
+
+
+
+
